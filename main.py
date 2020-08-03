78450d49ce6681463d9135f4896d62a9a035b91f
GrahamEmory <gemory44@gmail.com>
2020-08-03 21:33:47 +0000

    We added gameplay, options, questions, and other new features.

--- main.py
+++ main.py
@@ -16,3 +16,17 @@ Heroes = {
     "DBA" : "Hawkeye",
     "DBB" : "Black Widow"
 }
+
+name = input("Hello, what is your name?")
+answer = input("Hello "+ name +"! Would you like to play? Y or N")
+
+if answer.upper() == " Y":
+  question1 = input("What color do you prefer?(Choose A or B) \n A) Blue \n B) Red")
+  question2 = input("What activity would you much rather do with your friends?(Choose A or B) \n A) Go to the Movies \n B) Go shop at the mall")
+  question3 = input("What is your favorite dessert?(Choose A or B) \n A) Ice Cream \n B) Cake")
+  choice = question1 + question2 +question3
+  wait = input("Are you ready to know what Marvel Hero you are most like?")
+  if wait.upper() == " Y":
+    print("Your Marvel Hero is " + Heroes[choice.upper()])
+  else:
+    print("Okay")
